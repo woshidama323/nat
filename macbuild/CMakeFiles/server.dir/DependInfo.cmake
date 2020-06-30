4f7ef13d6730ffc164a72e70c9c54964aa2bdc4c
harry <harry.ma@topnetwork.org>
2020-06-30 16:33:25 +0800

    fix the cmakefile

---
+++ macbuild/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/imac/Documents/coderep/depnat/client.cpp" "/Users/imac/Documents/coderep/depnat/macbuild/CMakeFiles/server.dir/client.cpp.o"
+  "/Users/imac/Documents/coderep/depnat/dispatchqueue.cpp" "/Users/imac/Documents/coderep/depnat/macbuild/CMakeFiles/server.dir/dispatchqueue.cpp.o"
+  "/Users/imac/Documents/coderep/depnat/main.cpp" "/Users/imac/Documents/coderep/depnat/macbuild/CMakeFiles/server.dir/main.cpp.o"
+  "/Users/imac/Documents/coderep/depnat/managerhost.cpp" "/Users/imac/Documents/coderep/depnat/macbuild/CMakeFiles/server.dir/managerhost.cpp.o"
+  "/Users/imac/Documents/coderep/depnat/server.cpp" "/Users/imac/Documents/coderep/depnat/macbuild/CMakeFiles/server.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
